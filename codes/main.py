73c197d2cb51791b04f2b0d06a47c3f5b2725933
gy8 <garry.yangguang@gmail.com>
2014-09-29 05:51:55 -0700

    Add pretty print with section name

--- codes/main.py
+++ codes/main.py
@@ -5,18 +5,18 @@
 from solution_mult_choice import *  # NOQA
 import os.path
 
-GRADER = "Guang Yang (gy8@berkeley.edu)"
 
-
-def mult_choice(file_name):
+def mult_choice(file_name, score):
     """ automating the process for grading multiple choices
 
     while the user input is not "done", we ask user for 1) question number and
     2) error number. We combine these two strings into a dictionary key to pull
     data from the solutions
     """
+    add_section_name_to_file(file_name, 1)
     not_done = True
     solution = solution_mult_choice(LAB)
+    points_off = 5
 
     while not_done:
         question_num = input("(multiple choice) Enter question number"
@@ -30,18 +30,19 @@ def mult_choice(file_name):
             try:
                 feedback_content = solution[dict_key]
                 add_feedback_to_file(file_name, question_num,
-                                     feedback_content)
+                                     feedback_content, points_off)
                 print("Successfully added feedback {0}".format(dict_key))
+                score = score - 5
             except KeyError:
-                print("Not a valid key")
+                print("Not a valid dictionary key")
 
 
 def data_analysis():
     """ automating the process for grading data analysis """
 
 
-def add_feedback_to_file(file_name, question_num, feedback_content):
-    """ open existing file, append feedback, then close it
+def add_feedback_to_file(file_name, question_num, feedback_content, points_off):
+    """ open existing file, append feedback and points off, then close it
 
     Args:
         section (int):              student's section number
@@ -49,19 +50,30 @@ def add_feedback_to_file(file_name, question_num, feedback_content):
         s_name (list):              student's first and last name
         question_num (str):         question number and wrong answer choice
         feedback_content (str):     content of the feedback
+        points_off (int):           point to be taken off for the question
 
     Returns:
         None
 
     Example:
-        >>> add_feedback(3, 1, "Guang Yang", "1d", "Hahahaha noob")
+        >>> add_feedback("sec1_lab1_yang-guang", "1d", "Hahahaha noob", 5)
     """
     with open(file_name, 'a') as f:
-        f.write("\n-- {0} --\n{1}".format(question_num, feedback_content))
+        f.write("\n-- question {0} ({2} points) --"
+                "\n{1}\n".format(question_num, feedback_content, points_off))
 
 
-def add_section_name_to_file(section_name):
-    pass
+def add_section_name_to_file(file_name, section_num):
+    """ Adds section name to the file based on the section number given:
+        1 is multiple choice,
+        2 is data analysis,
+        3 is general questions
+    """
+    section_dict = {1: "\n\nMultiple Choices",
+                    2: "\n\nData Analysis",
+                    3: "\n\nAdditional Comments"}
+    with open(file_name, 'a') as f:
+        f.write(section_dict[section_num])
 
 
 def new_file(section, lab, s_name, file_name):
@@ -86,7 +98,10 @@ def generate_file_name(section, lab, s_name):
 if __name__ == "__main__":
     SECTION = 1
     LAB = 1
+    GRADER = "Guang Yang (gy8@berkeley.edu)"
     S_NAME = "Guang Yang"  # input("Student Name: ")
     FILE_NAME = generate_file_name(SECTION, LAB, S_NAME)
+    score = 100
+
     new_file(SECTION, LAB, S_NAME, FILE_NAME)
-    mult_choice(FILE_NAME)
+    score = mult_choice(FILE_NAME, score)
