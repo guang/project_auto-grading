fd1fad6f0f57c666b7fecd6c053f8dec60d312cb
gy8 <garry.yangguang@gmail.com>
2014-09-29 05:26:43 -0700

    Replace redundancies using the file name

--- codes/main.py
+++ codes/main.py
@@ -8,33 +8,46 @@ import os.path
 GRADER = "Guang Yang (gy8@berkeley.edu)"
 
 
-def mult_choice():
-    """ automating the process for grading multiple choices """
+def mult_choice(file_name):
+    """ automating the process for grading multiple choices
+
+    while the user input is not "done", we ask user for 1) question number and
+    2) error number. We combine these two strings into a dictionary key to pull
+    data from the solutions
+    """
     not_done = True
     solution = solution_mult_choice(LAB)
+
     while not_done:
-        prompt = ("Enter question number and wrong answer for multiple choice"
-                  "(i.e. 1c or 5a) \n")
-        feedback_num = input(prompt)
-        if feedback_num == 'done':
+        question_num = input("(multiple choice) Enter question number"
+                             "(or type done): ")
+        if question_num == 'done':
             not_done = False
+            print("Exiting multiple choice section")
         else:
+            error_num = input("(multiple choice) Enter error number: ")
+            dict_key = "{0}{1}".format(question_num, error_num)
             try:
-                feedback_content = solution[feedback_num]
-                add_feedback_to_file(SECTION, LAB, S_NAME, feedback_num,
+                feedback_content = solution[dict_key]
+                add_feedback_to_file(file_name, question_num,
                                      feedback_content)
+                print("Successfully added feedback {0}".format(dict_key))
             except KeyError:
                 print("Not a valid key")
 
 
-def add_feedback_to_file(section, lab, s_name, feedback_num, feedback_content):
+def data_analysis():
+    """ automating the process for grading data analysis """
+
+
+def add_feedback_to_file(file_name, question_num, feedback_content):
     """ open existing file, append feedback, then close it
 
     Args:
         section (int):              student's section number
         lab (int):                  student's lab number
         s_name (list):              student's first and last name
-        feedback_num (str):         question number and wrong answer choice
+        question_num (str):         question number and wrong answer choice
         feedback_content (str):     content of the feedback
 
     Returns:
@@ -43,17 +56,19 @@ def add_feedback_to_file(section, lab, s_name, feedback_num, feedback_content):
     Example:
         >>> add_feedback(3, 1, "Guang Yang", "1d", "Hahahaha noob")
     """
-    file_name = generate_file_name(section, lab, s_name)
     with open(file_name, 'a') as f:
-        f.write("\n-- {0} --\n{1}".format(feedback_num, feedback_content))
+        f.write("\n-- {0} --\n{1}".format(question_num, feedback_content))
 
 
-def new_file(section, lab, s_name):
+def add_section_name_to_file(section_name):
+    pass
+
+
+def new_file(section, lab, s_name, file_name):
     """ create new file based on student information if not already there """
     new_file_boilerplate = "Name: {0}\nSection: {1}\nLab: {2}\n".format(s_name,
                                                                         lab,
                                                                         section)
-    file_name = generate_file_name(section, lab, s_name)
     if os.path.isfile(file_name):
         return
     else:
@@ -72,5 +87,6 @@ if __name__ == "__main__":
     SECTION = 1
     LAB = 1
     S_NAME = "Guang Yang"  # input("Student Name: ")
-    new_file(SECTION, LAB, S_NAME)
-    mult_choice()
+    FILE_NAME = generate_file_name(SECTION, LAB, S_NAME)
+    new_file(SECTION, LAB, S_NAME, FILE_NAME)
+    mult_choice(FILE_NAME)
