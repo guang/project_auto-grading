2a8197eea08a8921a2436e556c66205788433a7f
gy8 <garry.yangguang@gmail.com>
2014-10-25 22:51:11 -0700

    Clean up pyc and rename db seed file

--- codes/db_seed.py
+++ codes/db_seed.py
@@ -210,22 +210,47 @@ def solution_mult_choice_lab2():
     """
     mc_sol = []
     # lab2 q1
-    mc_sol
+    mc_sol.append((2, 1, "b", "The correct answer is A. "
+                   "Bar graph is simple and adequately captures the "
+                   "information about different religious affliations"))
+    mc_sol.append((2, 1, "c", "The correct answer is A. "
+                   "Bar graph is simple and adequately captures the "
+                   "information about different religious affliations"))
+    mc_sol.append((2, 1, "d", "The correct answer is A. "
+                   "Bar graph is simple and adequately captures the "
+                   "information about different religious affliations"))
+    mc_sol.append((2, 1, "e", "The correct answer is A. "
+                   "Line charts show how data change over intervals of the "
+                   "same unit of measurement (often time). Since the question "
+                   "makes no mention of how their religious affiliations "
+                   "change over time, we cannot use line charts"))
+    mc_sol.append((2, 1, "f", "The correct answer is A. "
+                   "Line charts show how data change over intervals of the "
+                   "same unit of measurement (often time). Since the question "
+                   "makes no mention of how their religious affiliations "
+                   "change over time, we cannot use line charts"))
 
     # lab2 q2
-    mc_sol.append((1, 2, "b", "The correct answer is A. Dichotomous is binary"
-                   ", not an interval variable"))
-    mc_sol.append((1, 2, "c", "The correct answer is A. This is not clear "
-                   "from just using a binary variable"))
-    mc_sol.append((1, 2, "d", "The correct answer is A. This is not clear "
-                   "from just using a binary variable"))
-    mc_sol.append((1, 2, "e", "The correct answer is A. It's not clear what's "
-                   "conceptual vs operational"))
+    mc_sol.append((2, 2, "a", "The correct answer is C. "
+                   "The null hypothesis assumes the number of song names "
+                   "the student can recall is exactly 10. The alternative "
+                   "hypothesis (the claim) is that the number of song names "
+                   "the students can recall is greater than 10."))
+    mc_sol.append((2, 2, "b", "The correct answer is C. "
+                   "The null hypothesis assumes the number of song names "
+                   "the student can recall is exactly 10. The alternative "
+                   "hypothesis (the claim) is that the number of song names "
+                   "the students can recall is greater than 10."))
+    mc_sol.append((2, 2, "d", "The correct answer is C. "
+                   "The null hypothesis assumes the number of song names "
+                   "the student can recall is exactly 10. The alternative "
+                   "hypothesis (the claim) is that the number of song names "
+                   "the students can recall is greater than 10."))
 
     # lab2 q3
-    mc_sol.append((1, 3, "a", "The correct answer is D. Not true - std "
+    mc_sol.append((2, 3, "a", "The correct answer is D. Not true - std "
                    "indeed gets 'stretched' by outliers"))
-    mc_sol.append((1, 3, "b", "The correct answer is D. This was a little "
+    mc_sol.append((2, 3, "b", "The correct answer is D. This was a little "
                    "tricky - the problem here is the use of 'any population'. "
                    "Let's demonstrate this by a counterexample. Say we have "
                    "two distributions, X which is normal distribution with "
@@ -237,23 +262,23 @@ def solution_mult_choice_lab2():
                    "singleton, the mean is 5 and variance is 0. As a result, "
                    "The chance of your sample falling within one standard "
                    "deviation of the mean is 100%%."))
-    mc_sol.append((1, 3, "c", "The correct answer is D. Take the constant "
+    mc_sol.append((2, 3, "c", "The correct answer is D. Take the constant "
                    "to be 0 and it's very clear this is wrong"))
 
     # lab2 q4
-    mc_sol.append((1, 4, "a", "The correct answer is B. This answer doesn't "
+    mc_sol.append((2, 4, "a", "The correct answer is B. This answer doesn't "
                    "make sense as there is no quota"))
-    mc_sol.append((1, 4, "c", "The correct answer is B. Where is the social "
+    mc_sol.append((2, 4, "c", "The correct answer is B. Where is the social "
                    "network?"))
-    mc_sol.append((1, 4, "d", "The correct answer is B. You need "
+    mc_sol.append((2, 4, "d", "The correct answer is B. You need "
                    "probabilities to select clusters"))
-    mc_sol.append((1, 4, "e", "The correct answer is B. But the prompt said "
+    mc_sol.append((2, 4, "e", "The correct answer is B. But the prompt said "
                    "random"))
-    mc_sol.append((1, 4, "f", "The correct answer is B. What's systematic "
+    mc_sol.append((2, 4, "f", "The correct answer is B. What's systematic "
                    "about it?"))
 
     # lab2 q5
-    mc_sol.append((1, 5, "a", "The correct answer is C. "
+    mc_sol.append((2, 5, "a", "The correct answer is C. "
                    "If we let X ~ N(50,sigma^2), we see that P(X>70) can be "
                    "expressed using Z = (X-50)/sigma. So P(X>70) is the same a"
                    "s P((X-50)/sigma > (70-50)/sigma), which is P(Z>(20/sigma)"
@@ -262,7 +287,7 @@ def solution_mult_choice_lab2():
                    "Similarly if we let Y be 1/100 sum(X_j), j from 1 to 100 "
                    "we have then Y ~ N(50,sigma^2/100 and follow the above "
                    "calculations we arrive at the same answer"))
-    mc_sol.append((1, 5, "b", "The correct answer is C. "
+    mc_sol.append((2, 5, "b", "The correct answer is C. "
                    "If we let X ~ N(50,sigma^2), we see that P(X>70) can be "
                    "expressed using Z = (X-50)/sigma. So P(X>70) is the same a"
                    "s P((X-50)/sigma > (70-50)/sigma), which is P(Z>(20/sigma)"
@@ -271,7 +296,7 @@ def solution_mult_choice_lab2():
                    "Similarly if we let Y be 1/100 sum(X_j), j from 1 to 100 "
                    "we have then Y ~ N(50,sigma^2/100 and follow the above "
                    "calculations we arrive at the same answer"))
-    mc_sol.append((1, 5, "d", "The correct answer is C. "
+    mc_sol.append((2, 5, "d", "The correct answer is C. "
                    "If we let X ~ N(50,sigma^2), we see that P(X>70) can be "
                    "expressed using Z = (X-50)/sigma. So P(X>70) is the same a"
                    "s P((X-50)/sigma > (70-50)/sigma), which is P(Z>(20/sigma)"
@@ -282,59 +307,80 @@ def solution_mult_choice_lab2():
                    "calculations we arrive at the same answer"))
 
     # lab2 q6
-    mc_sol.append((1, 6, "a", "The correct answer is B. "
+    mc_sol.append((2, 6, "a", "The correct answer is B. "
                    "This is law of large numbers"))
-    mc_sol.append((1, 6, "c", "The correct answer is B. "
+    mc_sol.append((2, 6, "c", "The correct answer is B. "
                    "Well this is kind of obvious you don't need a theorem to "
                    "know this"))
-    mc_sol.append((1, 6, "d", "The correct answer is B. "
+    mc_sol.append((2, 6, "d", "The correct answer is B. "
                    "Not the central limit theorem"))
 
-    # q7
-    mc_sol.append((1, 7, "7b", "The correct answer is A. "
+    # lab2 q7
+    mc_sol.append((2, 7, "7b", "The correct answer is A. "
                    "Standard error of the mean is defined as s/sqrt(n) where n "
                    "is the sample size. So it will get smaller"))
-    mc_sol.append((1, 7, "7c", "The correct answer is A. "
+    mc_sol.append((2, 7, "7c", "The correct answer is A. "
                    "It should actually be larger"))
-    mc_sol.append((1, 7, "7d", "The correct answer is A. "
+    mc_sol.append((2, 7, "7d", "The correct answer is A. "
                    "Sampling doesn't alter inherent population distribution"))
-    mc_sol.append((1, 7, "7e", "The correct answer is A. "
+    mc_sol.append((2, 7, "7e", "The correct answer is A. "
                    "That's not what central limit theorem tells us"))
-    mc_sol.append((1, 7, "7f", "The correct answer is A. "
+    mc_sol.append((2, 7, "7f", "The correct answer is A. "
                    ""))
 
-    # q8
-    mc_sol.append((1, 8, "a", "The correct answer is D. "
+    # lab2 q8
+    mc_sol.append((2, 8, "a", "The correct answer is D. "
                    "All three statistics are 30"))
-    mc_sol.append((1, 8, "b", "The correct answer is D. "
+    mc_sol.append((2, 8, "b", "The correct answer is D. "
                    "Yup it's a constant distribution, which is unimodal"))
-    mc_sol.append((1, 8, "c", "The correct answer is D. "
+    mc_sol.append((2, 8, "c", "The correct answer is D. "
                    "Yup constant distributions have 0 variance/std"))
-    mc_sol.append((1, 8, "e", "The correct answer is D. "
+    mc_sol.append((2, 8, "e", "The correct answer is D. "
                    "There's no errors so sure it's 0"))
 
-    # q9
-    mc_sol.append((1, 9, "a", "The correct answer is B. "
+    # lab2 q9
+    mc_sol.append((2, 9, "a", "The correct answer is B. "
                    "Using Bayes theorem we can write P(H|A) = P(H) * P(A|H)/"
                    "P(A) Now we know that P(H)=0.01 (given), P(A|H)=1 (trick "
                    "coin has two heads so it always flips head), P(A)=0.51 "
                    "(trick coin has 2 heads)."))
-    mc_sol.append((1, 9, "c", "The correct answer is B. "
+    mc_sol.append((2, 9, "c", "The correct answer is B. "
                    "Using Bayes theorem we can write P(H|A) = P(H) * P(A|H)/"
                    "P(A) Now we know that P(H)=0.01 (given), P(A|H)=1 (trick "
                    "coin has two heads so it always flips head), P(A)=0.51 "
                    "(trick coin has 2 heads)."))
-    mc_sol.append((1, 9, "d", "The correct answer is B. "
+    mc_sol.append((2, 9, "d", "The correct answer is B. "
                    "Using Bayes theorem we can write P(H|A) = P(H) * P(A|H)/"
                    "P(A) Now we know that P(H)=0.01 (given), P(A|H)=1 (trick "
                    "coin has two heads so it always flips head), P(A)=0.51 "
                    "(trick coin has 2 heads)."))
-    mc_sol.append((1, 9, "e", "The correct answer is B. "
+    mc_sol.append((2, 9, "e", "The correct answer is B. "
                    "Using Bayes theorem we can write P(H|A) = P(H) * P(A|H)/"
                    "P(A) Now we know that P(H)=0.01 (given), P(A|H)=1 (trick "
                    "coin has two heads so it always flips head), P(A)=0.51 "
                    "(trick coin has 2 heads)."))
 
+    # lab2 q10
+    mc_sol.append((2, 9, "a", "The correct answer is B. "
+                   "Using Bayes theorem we can write P(H|A) = P(H) * P(A|H)/"
+                   "P(A) Now we know that P(H)=0.01 (given), P(A|H)=1 (trick "
+                   "coin has two heads so it always flips head), P(A)=0.51 "
+                   "(trick coin has 2 heads)."))
+    mc_sol.append((2, 9, "c", "The correct answer is B. "
+                   "Using Bayes theorem we can write P(H|A) = P(H) * P(A|H)/"
+                   "P(A) Now we know that P(H)=0.01 (given), P(A|H)=1 (trick "
+                   "coin has two heads so it always flips head), P(A)=0.51 "
+                   "(trick coin has 2 heads)."))
+    mc_sol.append((2, 9, "d", "The correct answer is B. "
+                   "Using Bayes theorem we can write P(H|A) = P(H) * P(A|H)/"
+                   "P(A) Now we know that P(H)=0.01 (given), P(A|H)=1 (trick "
+                   "coin has two heads so it always flips head), P(A)=0.51 "
+                   "(trick coin has 2 heads)."))
+    mc_sol.append((2, 9, "e", "The correct answer is B. "
+                   "Using Bayes theorem we can write P(H|A) = P(H) * P(A|H)/"
+                   "P(A) Now we know that P(H)=0.01 (given), P(A|H)=1 (trick "
+                   "coin has two heads so it always flips head), P(A)=0.51 "
+                   "(trick coin has 2 heads)."))
     return mc_sol
 
 
@@ -649,15 +695,6 @@ def solution_mult_choice(lab):
     return mc_sol
 
 
-# def solution_data_analysis_lab1():
-
-
-def solution_data_analysis(lab):
-    """ solution bank for data analysis section for all 4 labs
-
-    """
-
-
 def write_mc_to_db(lab):
     """ writes mc_sol to database "solutions.db"
     Args:
@@ -673,4 +710,4 @@ def write_mc_to_db(lab):
 
 
 if __name__ == "__main__":
-    write_mc_to_db(1)
+    write_mc_to_db(2)
